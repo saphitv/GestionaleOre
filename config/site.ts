8f82a4f4ebee90c318f6c66516ce6bf9beeedaf6
saphi <simonmaggini@gmail.com>
2023-06-21 13:02:07 +0200

    Cambiato nome progetto

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Gestionale Ore",
   description:
     "Beautifully designed components built with Radix UI and Tailwind CSS.",
   mainNav: [
